326569915ba90be1f4b8a5b8f7cc7fa47f43bc37
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-08-10 23:51:01 +0300

    Populate cache during ServiceWorker installation

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
+var CACHE_NAME = "html5-qr-code-scanner-cache-v1";
+
 self.addEventListener('install', function(event) {
     // Perform install steps
     console.log("Installing ServiceWorker");
+
+    // event.waitUntil waits till the passed promise is resolved
+    // The URLs retrieval happens asynchronously so we have to wait until
+    // all of them have been retrieved and results stored in cache.
+    // Else ServiceWorker will be considered installed before cache is populated.
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log("Opened cache " + CACHE_NAME);
+
+                // Retrieve a passed array of URLs, and save responses in cache
+                return cache.addAll([
+                    "index.html",
+                    "jsqrcode.js",
+                    "https://fonts.googleapis.com/css?family=Roboto:100,400"
+                ]);
+            })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
     // Handle HTTP requests coming from the page
-    console.log("Fetching " + event.request);
+    console.log("Fetching " + event.request.url);
     event.respondWith(fetch(event.request));
 });
 
